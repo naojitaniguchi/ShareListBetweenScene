763c3e93b6f450f79a6178fbca092639b471ec2b
naojitaniguchi <naoji.taniguchi@gmail.com>
2020-12-03 21:50:36 +0900

    First commit.

---
+++ Assets/Scripts/GetSetValues.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetSetValues : MonoBehaviour
+{
+    public string globalValueTag = "GlobalVale";
+    public string stringCollectionTag = "StringCollection";
+    GameObject valuesObj;
+    GameObject stringCollectionObj;
+    Values values;
+    Fungus.StringCollection stringCollection;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        valuesObj = GameObject.FindGameObjectWithTag(globalValueTag);
+        values = valuesObj.GetComponent<Values>();
+
+        stringCollectionObj = GameObject.FindGameObjectWithTag(stringCollectionTag);
+        stringCollection = stringCollectionObj.GetComponent<Fungus.StringCollection>();
+
+        for (int i = 0; i < values.myList.Count; i++)
+        {
+            Debug.Log(values.myList[i]);
+        }
+
+        Debug.Log(values.flags[(int)Values.FLAGS.FLAG_0]);
+        Debug.Log(values.flags[(int)Values.FLAGS.FLAG_1]);
+        Debug.Log(values.flags[(int)Values.FLAGS.FLAG_2]);
+
+        setValuesToStringCollection();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setValuesFromStringCollection()
+    {
+        for ( int i = 0; i < stringCollection.Count; i ++)
+        {
+            Debug.Log((string)stringCollection.Get(i));
+
+            values.myList.Add((string)stringCollection.Get(i));
+        }
+    }
+
+    public void setValuesToStringCollection()
+    {
+        for (int i = 0; i < values.myList.Count; i++)
+        {
+            stringCollection.Add(values.myList[i]);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Values.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Values : MonoBehaviour
+{
+    public List<string> myList = new List<string>();
+    public enum FLAGS
+    {
+        FLAG_0,
+        FLAG_1,
+        FLAG_2,
+        FLAG_MAX,
+    }
+    public bool[] flags = new bool[(int)FLAGS.FLAG_MAX];
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            myList.Add("a");
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            myList.Add("b");
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            myList.Add("c");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            flags[(int)FLAGS.FLAG_0] = !flags[(int)FLAGS.FLAG_0];
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            flags[(int)FLAGS.FLAG_1] = !flags[(int)FLAGS.FLAG_1];
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            flags[(int)FLAGS.FLAG_2] = !flags[(int)FLAGS.FLAG_2];
+        }
+    }
+}
