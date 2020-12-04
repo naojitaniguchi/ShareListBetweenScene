b78d5d4f8cde0cefeef535ad3c827116dff7fbdc
naojitaniguchi <naoji.taniguchi@gmail.com>
2020-12-04 14:55:30 +0900

    added static sample.

---
+++ Assets/Scripts/SetVariableToFlowchartFromGlobal.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetVariableToFlowchartFromGlobal : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setFlagA()
+    {
+        gameObject.GetComponent<Fungus.Flowchart>().SetBooleanVariable("FlagA", GlobalVariable.flagA);
+    }
+
+    public void setFlagB()
+    {
+        gameObject.GetComponent<Fungus.Flowchart>().SetBooleanVariable("FlagB", GlobalVariable.flagB);
+    }
+
+    public void setFlagC()
+    {
+        gameObject.GetComponent<Fungus.Flowchart>().SetBooleanVariable("FlagC", GlobalVariable.flagC);
+    }
+
+}
---
+++ Assets/Scripts/SetVariablesToGlobalFromFlowchart.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetVariablesToGlobalFromFlowchart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FlagAOn()
+    {
+        GlobalVariable.flagA = true;
+    }
+
+    public void FlagAOff()
+    {
+        GlobalVariable.flagA = false;
+    }
+
+    public void FlagBOn()
+    {
+        GlobalVariable.flagB = true;
+    }
+
+    public void FlagBOff()
+    {
+        GlobalVariable.flagB = false;
+    }
+
+    public void FlagCOn()
+    {
+        GlobalVariable.flagC = true;
+    }
+
+    public void FlagCOff()
+    {
+        GlobalVariable.flagC = false;
+    }
+
+}
